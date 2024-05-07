c3a84540c41ca0657e420e17db3f79f9b83df6b1
OriolBoladerasATM <oboladeras@automaticmedia.tv>
2024-05-07 19:58:30 +0200

    F1 update

---
+++ static/f1/frontend.js
@@ -0,0 +1,96 @@
+function createDriver(driver) {
+    var driverDiv = document.createElement('div');
+    driverDiv.className = 'driver';
+
+    var position = document.createElement('h2');
+    position.className = 'position';
+    position.textContent = driver.position;
+
+    var logo = document.createElement('img');
+    logo.src = "/static/f1/logos/" + driver.logo;
+    logo.alt = 'logo';
+
+    var name = document.createElement('h2');
+    name.textContent = driver.name;
+    name.className = 'name';
+
+    var time = document.createElement('h3');
+    time.textContent = driver.time;
+    time.className = 'time';
+
+    driverDiv.appendChild(position);
+    driverDiv.appendChild(logo);
+    driverDiv.appendChild(name);
+    driverDiv.appendChild(time);
+
+    return driverDiv;
+}
+
+function getDrivers() {
+    grid = document.getElementById('grid');
+
+    fetch('/api/f1/' + id, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }).then(response => response.json())
+        .then(data => {
+            for (var i = 0; i < data.drivers.length; i++) {
+                grid.appendChild(createDriver(data.drivers[i]));
+            }
+        })
+}
+
+function updateTimer(targetTime) {
+    timer = document.getElementById('timer');
+
+    var currentTime = new Date();
+    var remainingTime = targetTime - currentTime;
+    console.log(remainingTime);
+
+    if (remainingTime <= 0) {
+        clearInterval(timerInterval);
+        document.getElementById("timer").textContent = "Time's up!";
+        return;
+    }
+
+    // var hours = Math.floor(remainingTime / (1000 * 60 * 60));
+    var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+    minutes = minutes.toString().padStart(2, '0');
+    seconds = seconds.toString().padStart(2, '0');
+
+    document.getElementById("timer").textContent = minutes + ":" + seconds;
+
+    setTimeout(() => {
+        updateTimer(targetTime);
+    }, 1000);
+}
+
+
+
+fetch('/api/f1/' + id, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+}).then(response => response.json())
+    .then(data => {
+        timer = document.getElementById('timer');
+
+        var timeParts = data.endTime.split(":");
+        var hours = timeParts[0];
+        var minutes = timeParts[1];
+
+        var targetTime = new Date();
+        targetTime.setHours(hours);
+        targetTime.setMinutes(minutes);
+
+        updateTimer(targetTime);
+    })
+
+
+
+getDrivers();
