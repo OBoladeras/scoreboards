23e650318424da38c0a39b48f5f6ee613bca198d
OBoladeras <oriolboladeras@gmail.com>
2024-05-14 11:09:33 +0200

    Optimization

--- static/volleyball/frontend.js
+++ static/volleyball/frontend.js
@@ -13,6 +13,8 @@ points_team2 = document.getElementById('points_team2');
 sets_team1 = document.getElementById('sets_team1');
 sets_team2 = document.getElementById('sets_team2');
 
+logo1 = "volei-sant-cugat-b.png";
+logo2 = "sant-boi-voley.png";
 
 function update() {
     fetch('/api/volleyball/' + id, {
@@ -26,8 +28,13 @@ function update() {
         name_team1.innerHTML = data.team1.name;
         name_team2.innerHTML = data.team2.name;
 
-        logo_team1.src = `/static/volleyball/logos/${data.team1.logo}`;
-        logo_team2.src = `/static/volleyball/logos/${data.team2.logo}`;
+        if (data.team1.logo != logo1 && data.team1.logo != logo2) {
+            logo_team1.src = `/static/volleyball/logos/${data.team1.logo}`;
+            logo_team2.src = `/static/volleyball/logos/${data.team2.logo}`;
+
+            logo1 = data.team1.logo;
+            logo2 = data.team2.logo;
+        }
 
         changes_team1.innerHTML = data.team1.changes;
         changes_team2.innerHTML = data.team2.changes;
